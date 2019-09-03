272eafcad4aaff6eceb9eadc95fc934f88fa8a4b
Brian Blaylock <u0553130@ad.utah.edu>
2019-09-03 11:38:29 -0600

    updates

---
+++ first_python_script.py
@@ -0,0 +1,23 @@
+# Brian Blaylock
+# September 3, 2019
+
+print('Hello World!')
+
+a = 10
+b = 15
+c = 'This is a string'
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print('Add a+b:', a+b)
+
+print(c)
+
+# You can add strings to strings but not to integers or floats.
+print(c+'. And this was added to that string.')
+
+# This will result in an error (what does the error message say?)
+print(c+a)
+
